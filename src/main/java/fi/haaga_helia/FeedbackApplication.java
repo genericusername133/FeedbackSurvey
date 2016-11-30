3de4bd582b510f5ed203c96f9c3de15d7c2c090a
a1503045 <a1503045@PAO5006XL28.haagahelia.amk>
2016-11-30 11:23:00 +0200

    CRUDia väsätty

--- src/main/java/fi/haaga_helia/FeedbackApplication.java
+++ src/main/java/fi/haaga_helia/FeedbackApplication.java
@@ -2,11 +2,19 @@ package fi.haaga_helia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
-public class FeedbackApplication {
+public class FeedbackApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FeedbackApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FeedbackApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(FeedbackApplication.class, args);
+    }
+
 }
