ba66a53574e0829233f52a4426381ed6fbab935e
Laurasvaara Inka <a1502992@myy.haaga-helia.fi>
2016-12-02 12:40:21 +0200

    Boot Security 1.1

--- src/main/java/fi/haaga_helia/repository/UserRepository.java
+++ src/main/java/fi/haaga_helia/repository/UserRepository.java
@@ -1,7 +1,9 @@
-package fi.haaga_helia.entity;
+package fi.haaga_helia.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import fi.haaga_helia.entity.User;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
--- src/main/java/fi/haaga_helia/service/UserDetailServiceImpl.java
+++ src/main/java/fi/haaga_helia/service/UserDetailServiceImpl.java
@@ -1,13 +1,14 @@
 package fi.haaga_helia.service;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
 import fi.haaga_helia.entity.User;
-import fi.haaga_helia.entity.UserRepository;
+import fi.haaga_helia.repository.UserRepository;
+
 
 
 
